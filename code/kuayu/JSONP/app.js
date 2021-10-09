24f9204050f57ea9a423fd9df1fef666d9956aa1
2233282817@qq.com <2233282817@qq.com>
2021-10-09 21:44:10 +0800

    跨域

---
+++ code/kuayu/JSONP/app.js
@@ -0,0 +1,12 @@
+const data={
+    name:'cjs-shuai'
+};
+// console.log(data);
+//处理函数
+/* function handle(data){
+    //获取result元素
+    const result=document.getElementById('result');
+    result.innerHTML=data.name;
+} */
+
+handle(data);
--- code/server.js
+++ code/server.js
@@ -96,6 +96,49 @@ app.all('/fetch-server',(request,response)=>{
   
 });
 
+//jsonp服务
+app.all('/jsonp-server',(request,response)=>{ 
+    // response.send('hello jsonp-server'); 这个返回结果js不能解析
+    // response.send('console.log("hello cjs")'); 这样就能解析
+    const data={
+        name:'cjs ooo'
+    };
+    //将数据转化为字符串
+    let str = JSON.stringify(data);
+    //返回结果
+    response.end(`handle(${str})`);
+});
+
+//jsonp服务
+app.all('/check-username',(request,response)=>{ 
+    // response.send('hello jsonp-server'); 这个返回结果js不能解析
+    // response.send('console.log("hello cjs")'); 这样就能解析
+    const data={
+        exist:1,
+        msg:'用户名已经存在'
+    };
+    //将数据转化为字符串
+    let str = JSON.stringify(data);
+    //返回结果
+    response.end(`handle(${str})`);
+});
+
+//jsonp服务
+app.all('/jquery-jsonp-server',(request,response)=>{ 
+    // response.send('hello jsonp-server'); 这个返回结果js不能解析
+    // response.send('console.log("hello cjs")'); 这样就能解析
+    const data={
+        name:'cjs',
+        city:['北京','天津','秦皇岛']
+    };
+    //将数据转化为字符串
+    let str = JSON.stringify(data);
+    //接收callback参数 这个就是html中url后面那个callback方法.
+    let cb=request.query.callback;
+    //返回结果 这里相当于调用了callback方法.
+    response.end(`${cb}(${str})`);
+});
+
 // 4.监听端口启动服务
 app.listen(8000,()=>{
     console.log('服务已经启动,8000端口监听中....');
